d8e7eb5a4062d3a87e3ef2dd20b63e92d2e0999d
Denis O'Riordan <denis.oriordan1@ucdconnect.ie>
2021-10-23 14:31:24 +0100

    Finalised Version of Inverse Hyperbolic Tangent Function (InvHypTan.c) complete. Working with comments

--- practical05/InvHypTan.c
+++ practical05/InvHypTan.c
@@ -1,44 +1,75 @@
 #include <stdio.h>
 #include <math.h>
 
-float arctanh1(float x);
-float arctanh2(float y);
 
+// declare function to calculate hyperbolic arc tangent using MacLaurin Series
+double arctanh1(const double x, const double delta);
+// function to calculate hyperbolic arc tangent using natural logarithims
+double arctanh2( const double x);
 
-int main(void) {
+// main function
+int main() {
 
-float delta, error, result;
-printf("Please input a positive real number delta:\n");
-error = scanf("%f",&delta);
-   if (delta <=0) {
-     printf("This is not positive!\n");
-     return 1;
-   }
-printf("%f\n", delta);
+// input delta - a positive real number which will be used as the percision for the McLaurin Series
+  double delta ;
+  printf("Enter precision for the Maclaurin Series:\n");
+  scanf("%lf", &delta);
+  
+// input begin, end - the range over x the hyperbolic arc tangent will be calculated
+  double begin, end;
+  printf("Enter the start and finish points for x:\n");
+  scanf("%lf, %lf", &begin, &end);
 
+// define size of the arrays for where the tan values are calculated and stored during the loop
+// create a array of correct shape, fabs() due to possible negative value for x
+  double a =  ((fabs(begin) + fabs(end))/delta) + 1.;
+// cast as integer to create arrays
+  int asize = a;
 
-result = arctanh1(0.9);
-printf("\n%f\n", result);
+// declare arrays
+  double tan1[asize];
+  double tan2[asize];
 
-return 0;
-}
+//main component of loop, is double as begining and end values of x, delta are not int's
+  double i;
+//index in array
+  int j = 0;
 
-// arctanh(x) function
+// forloop: stores tan value in array (both methods) and prints difference for each iteration
+  for ( i = begin; i <= end;  i += delta) {
+    tan1[j] = arctanh1(i, delta);
+    tan2[j] = arctanh2(i);
+    printf("The difference between tan1[%.3lf] and tan2[%.3lf] is %.10lf.\n", i, i , fabs(tan1[j] - tan2[j]));
+    j++;
 
-float arctanh1(float x) {
-  float sum = 0, delta = 1.29; 
-  int n = 0;
-  while (sum < delta) {
-	sum = sum + (pow(x,((2*n)+1)))/((2*n+1));
-	n++;
   }
-  return sum;
+
+
+  return 0;
 }
 
+// function to calculate inverser hyperbolic tan using MacLaurin Series
+double arctanh1(const double x, const double delta){
+// arcTan: value returned at end of function
+  double arcTan = 0;
+// elem: element in series, redefined for each value of n
+  double elem;
+// n integer n to iterate over
+  int n = 0;
+// calculate element value and add to arcTan value
+// repeat for n++ until the absolute value of the element is less than the precision  delta
+  do {
+	double val = 2*n + 1;
+	elem = pow(x, val)/val;
+	arcTan += elem;
+	n++;
+  }  while(fabs(elem) >= delta);
+  return arcTan;
+}
 
-//approximate function 
+// function to calculate inverser hyperbolic tan using natural logarithims. Formula given below.
+double arctanh2( const double x){
+  return (log(1 + x) - log(1-x))/2;
 
-
-
-
+}
 
