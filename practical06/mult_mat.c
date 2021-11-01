00c4bd610e0afd22d56cd9dbf5e86aa241041af2
Denis O'Riordan <denis.oriordan1@ucdconnect.ie>
2021-11-01 10:02:07 +0000

    matrix multiplication file added to practical06, first commit

---
+++ practical06/mult_mat.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+int main(){
+
+  int n = 5;
+  int  p=3; 
+  int q=4;
+  double A[n][p], B[p][q], C[n][q];
+  int i, j, k;
+
+  //Initialise A, B and C matrices
+  //A
+  for (i=0; i<n; i++){
+    for (j=0; j<p; j++) {
+      A[i][j]=i+j;	
+    }  
+  }
+  //B
+  for (i=0 ; i<p ; i++){
+        for( j=0 ; j < q ; j++) {
+                B[i][j] = i-j;
+        }
+  }
+  //C
+  for (i=0 ; i<n ; i++){
+        for( j=0 ; j < q ; j++) {
+                C[i][j] = 0.0;
+        }
+  }
+  
+  //Matrix Multiplication
+  for (i=0 ; i<n ; i++)
+        for( j=0 ; j < q ; j++)
+                for ( k=0 ; k < p ; j++)
+		C[i][j] = C[i][j] + A[i][k] + B[k][j];
+  
+  //print the matrices
+  printf("\n The matrix A is:\n");
+  for (i=0 ; i<n ; i++){
+    for( j=0 ; j < p ; j++) {
+      printf(" %3.0f ", A[i][j]);
+  } 
+  printf("\n");
+  }
+ 
+  printf("\n The matrix B is:\n");
+  for (i=0 ; i<p ; i++){
+        for( j=0 ; j < q ; j++) {
+                printf(" %3.0f ", B[i][j]);
+        }
+  printf("\n");
+  }
+  
+  printf("\n The matrix C is:\n");
+  for (i=0 ; i<n ; i++){
+        for( j=0 ; j < q ; j++) {
+                printf(" %3.0f ", C[i][j]);
+        }
+  printf("\n");
+  }
+
+
+
+
+
+ return 0;
+} 
