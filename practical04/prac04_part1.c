ce68b844e6f782b4cb4bb069cd688231cdbef271
Denis O'Riordan <denis.oriordan1@ucdconnect.ie>
2021-11-04 17:15:59 +0000

    Comment tweaks to prac04_part1.c. Final commit

--- practical04/prac04_part1.c
+++ practical04/prac04_part1.c
@@ -9,24 +9,18 @@ float x_deg[12], x_rad[12] , y[12];
 
 int main(void) {
 
-// Declare Variables
-   float a = 0.0 , b_deg = 60.0, b_rad;
-// Use our function degtorad
-   b_rad = degtorad(b_deg);
 //creating and printing an array of tan(x)
-
+//x_rad array of radian values using degtorad function and degree values of 0,5,10,..,60
    int i;
    printf("\nWhere x in is radians");
    for (i=0; i<13; i++) {
-	x_deg[i] = i*5;
-	x_rad[i] = degtorad(x_deg[i]);
+	x_rad[i] = degtorad(i*5);
 	y[i] = tan(x_rad[i]);
 	printf("\nTan(%f) = %f",x_rad[i], y[i] );
    }
    printf("\n");
 return 0;
 }
-
 
 // degtorad : converts degrees to radians of arg
    double degtorad(double arg) {
