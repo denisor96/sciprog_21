c9051340f1205d3e9a644f1c5c2a183fc12bf199
Denis O'Riordan <denis.oriordan1@ucdconnect.ie>
2021-11-04 16:00:48 +0000

    Three updates. 1 - added readme file to give overview of practical file, first commit. 2 - Edited commenting on traprule.c . 3 - Removed traprulePRAC.c,, version of practical exercise from weekly video, do not require two versions of same function.

---
+++ practical03/README.txt
@@ -0,0 +1,12 @@
+This weeks practical contains one exercise comprising of one file
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+traprule.c
+- This program requires no input from the user
+- This c file calculates the area under the curve of tan(x) from 0 to pi/3 (in radians) using the trapezoidal rule.
+- It also calculates the exact value for this area under the curve of tan(x) from 0 to pi/3 (in radians) which conveniently is log(2).
+- This function creates 11 equidistant points between the beginning and end values and prints the x an y values for each increment.
+- It gets f(xi) for each point from x0 (0) to x12 (pi/3)
+- It then sums the values as per the function and applies the (b-a)/N factor to generate the trapexoidal rule estimate.
+- Finally, it prints both values for the area under the curve and the difference between the two.
+-----------------------------------------------------------------------------------------------------------------------------------------
--- practical03/traprule.c
+++ practical03/traprule.c
@@ -2,44 +2,49 @@
 #include <math.h>
 
 int main(void) {
-/* Declare variables */
+// Declare variables
    int i , N;
    float  a, b, sum, logtwo, diff , x[12], y[12];
   
-/* 1. Declaring and printing our start/end points */
+// Declaring and printing our start/end points 
    a = 0;
    b = M_PI/3;
 
    printf("We are integrating from %.0f to %.5f\n", a, b);
 
-/* 2. Getting the function values i.e. tan(0) & tan(pi/3) for our start and end points */   
+// Getting the function values i.e. tan(0) & tan(pi/3) for our start and end points
    printf("Tan of %.0f is %.5f\n", a, tan(a));
    printf("Tan of %.5f is %.5f\n", b, tan(b));
 
-/* 3. Generate 11 equidistant points between a and b */
+// Generate 11 equidistant points between a and b
    for (i=0; i<=12; i++) {
       x[i] = a + i*(b/12);
       y[i] = tan(x[i]);  
-      printf("\n x%d and y%.d are %.5f and %.5f \n", i, i ,  x[i], y[i]);
+      printf("\n x%d and y%d are %.5f and %.5f", i, i ,  x[i], y[i]);
    }
-/* Get 2* our y value from y[1] to y[11] */
+
+// Get 2* our y value from y[1] to y[11]
    for (i =1; i<=11; i++) {
 	y[i] = 2* y[i];
    }
 
-/* Sum from i=0 to i=12 */
+// Sum from i=0 to i=12
    sum = 0.0;
    for (i=0; i<=12; i++) {
 	sum = sum + y[i];
    }
 
-/* 4. Get the correct result i.e. the log of two */
-   N=12;
+// apply the (b-a)/2N factor to the sum to get our estimation via trapezoidal rule
+   N= 12;
    sum = sum*((b-a)/(2*N));
+   logtwo = log(2);
+
+// Get the correct result i.e. the log of two and its difference from the trapezoidal rule (sum)
    logtwo = log(2);
    diff = logtwo - sum;
 
-/* 5. Print final results and compare answers */ 
+// Print final results and compare answers
+   printf("\n");
    printf("\n The area under the curve using the Trapezoidal rule is %.5f", sum);
    printf("\n The correct area under the curve is %.5f", logtwo);
    if (diff > 0) {
