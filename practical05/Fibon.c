656e810e3bae71eabd136b9632b56c25e96310d2
Denis O'Riordan <denis.oriordan1@ucdconnect.ie>
2021-11-08 19:41:11 +0000

    updated comments on both files. Final commit.

--- practical05/Fibon.c
+++ practical05/Fibon.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-// Declaring the fibonacci function
+//fibo: perform main operation of a Fibonacci Sequence 
+// two inputs (*a, *b) - converts *a = *b and *b = *a + *b  (orginal values)
 void fibo(int *a, int *b);
 
-//main function
 int main() {
 
-//Inputting the value for N ~ number of values on Fibonnacci Sequence I want printed 
+//Inputting the value for N ~ number of values on Fibonnacci Sequence to be printed
+//Exit function if <1
   int N;
   printf("\nPlease input a positive integer N:\n");
   scanf("%d",&N);
-// Testing input is >1
    if (N <1) {
      printf("This is less than 1!\n");
      return 1;
    }
-// Declaring initial input values for Fibonnacci function  
+
+// Declaring initial input values for Fibonnacci function - always 0,1 
   int N1 = 0;
   int N2 = 1;
 
-// Print info for context
+// Print info. for context and initial value as will be altered once loop begins
   printf("\nThe number you have entered is %d\n", N);
-  printf("The Fibonacci Sequence for %d is: \n", N);
-// Print initial value of sequence i.e. 0 
- printf("%d \n", N1);
+  printf("The Fibonacci Sequence for %d is: \n", N); 
+  printf("%d \n", N1);
+
 // For Loop to run and print fibonacci function up to (n-1)th value  
   int i ;
   for (i=1; i < N-1 ; i++) {
 	fibo(&N1, &N2);
 	printf("%d \n", N1) ;
-
   }
+
 // Run and print fibonnacci function for Nth and final time 
   fibo(&N1, &N2);
   printf("%d \n", N1);
@@ -39,10 +40,8 @@ int main() {
  return 0;
 }
 
-// Fibonacci function which takes two inputs *a, *b
-// sums *a and *b
-// assigns *a the value of b
-// assigns *b the value of the sum
+//fibo: perform main operation of a Fibonacci Sequence
+// two inputs (*a, *b) - converts *a = *b and *b = *a + *b  (orginal values)
 void fibo(int *a, int *b) {
 	int next;
 	next = *a + *b;
--- practical05/InvHypTan.c
+++ practical05/InvHypTan.c
@@ -2,74 +2,74 @@
 #include <math.h>
 
 
-// declare function to calculate hyperbolic arc tangent using MacLaurin Series
+// arctanh1: calculate hyperbolic arc tangent using MacLaurin Series
 double arctanh1(const double x, const double delta);
-// function to calculate hyperbolic arc tangent using natural logarithims
+// arctanh2: calculate hyperbolic arc tangent using natural logarithims
 double arctanh2( const double x);
 
-// main function
+
 int main() {
 
-// input delta - a positive real number which will be used as the percision for the McLaurin Series
-  double delta ;
-  printf("Enter precision for the Maclaurin Series:\n");
-  scanf("%lf", &delta);
-  
-// input begin, end - the range over x the hyperbolic arc tangent will be calculated
-  double begin, end;
-  printf("Enter the start and finish points for x:\n");
-  scanf("%lf, %lf", &begin, &end);
+   // input delta, begin & end
+   // delta - a positive real number which will be used as the percision for the McLaurin Series
+   // begin, end - the range over x the hyperbolic arc tangent will be calculated
+   double delta ;
+   double begin, end;
+   printf("Enter precision for the Maclaurin Series:\n");
+   scanf("%lf", &delta);
+   printf("Enter the start and finish points for x:\n");
+   scanf("%lf, %lf", &begin, &end);
 
-// define size of the arrays for where the tan values are calculated and stored during the loop
-// create a array of correct shape, fabs() due to possible negative value for x
-  double a =  ((fabs(begin) + fabs(end))/delta) + 1.;
-// cast as integer to create arrays
-  int asize = a;
+   // define size of the arrays for where the tan values are calculated and stored during the loop
+   // fabs() due to possible negative value for x
+   // cast as integer and initialize arrays to store answers for both methods
+   double a =  ((fabs(begin) + fabs(end))/delta) + 1.;
+   int asize = a;
+   double tan1[asize];
+   double tan2[asize];
 
-// declare arrays
-  double tan1[asize];
-  double tan2[asize];
+   //main component of loop, is double as begining and end values of x, delta are not int's
+   double i;
+   //index in array
+   int j = 0;
 
-//main component of loop, is double as begining and end values of x, delta are not int's
-  double i;
-//index in array
-  int j = 0;
+   // forloop: stores tan value in array (both methods) and prints difference for each iteration
+   for ( i = begin; i <= end;  i += delta) {
+     tan1[j] = arctanh1(i, delta);
+     tan2[j] = arctanh2(i);
+     printf("The difference between tan1[%.3lf] and tan2[%.3lf] is %.10lf.\n", i, i , fabs(tan1[j] - tan2[j]));
+     j++;
 
-// forloop: stores tan value in array (both methods) and prints difference for each iteration
-  for ( i = begin; i <= end;  i += delta) {
-    tan1[j] = arctanh1(i, delta);
-    tan2[j] = arctanh2(i);
-    printf("The difference between tan1[%.3lf] and tan2[%.3lf] is %.10lf.\n", i, i , fabs(tan1[j] - tan2[j]));
-    j++;
+   }
 
-  }
-
-
-  return 0;
+   return 0;
 }
 
-// function to calculate inverser hyperbolic tan using MacLaurin Series
+
+// arctanh1: calculate hyperbolic arc tangent using MacLaurin Series
 double arctanh1(const double x, const double delta){
-// arcTan: value returned at end of function
-  double arcTan = 0;
-// elem: element in series, redefined for each value of n
-  double elem;
-// n integer n to iterate over
-  int n = 0;
-// calculate element value and add to arcTan value
-// repeat for n++ until the absolute value of the element is less than the precision  delta
-  do {
+
+   // arcTan: value returned at end of function
+   // elem: element in series, redefined for each value of n
+   // n: integer n to iterate over
+   double arcTan = 0;
+   double elem;
+   int n = 0;
+
+   // calculate element value using formula given and add to arcTan value
+   // repeat for n++ until the absolute value of the element is less than the precision  delta
+   do {
 	double val = 2*n + 1;
 	elem = pow(x, val)/val;
 	arcTan += elem;
 	n++;
-  }  while(fabs(elem) >= delta);
-  return arcTan;
+   }  while(fabs(elem) >= delta);
+   return arcTan;
 }
 
-// function to calculate inverser hyperbolic tan using natural logarithims. Formula given below.
-double arctanh2( const double x){
-  return (log(1 + x) - log(1-x))/2;
+// arctanh2: calculate hyperbolic arc tangent using natural logarithims
+double arctanh2(const double x){
+   return (log(1 + x) - log(1-x))/2;
 
 }
 
